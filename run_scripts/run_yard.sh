f47f36d58e94acb35a400ab0acde4842095873e7
liyi193328 <liyi193328@github.com>
2017-05-22 13:28:14 +0800

    fix yard

--- run_scripts/run_yard.sh
+++ run_scripts/run_yard.sh
@@ -10,8 +10,11 @@ echo $4
 
 ##changable according to your data dir and task
 
+echo "must enter the project seq2seq dir, then bash run_scripts/local_yard_test.sh"
+
 DATA_ROOT=/mnt/yardcephfs/mmyard/g_wxg_td_prc/turingli/data
-SEQ2SEQ_PROJECT_DIR=${PWD}/seq2seq
+
+SEQ2SEQ_PROJECT_DIR=${PWD}
 CONFIG_DIR=${SEQ2SEQ_PROJECT_DIR}/example_configs/yard_ques_gen_10w_config
 echo "config dir: ${CONFIG_DIR}"
 echo "seq2seq project dir: ${SEQ2SEQ_PROJECT_DIR}"
@@ -27,6 +30,7 @@ DEV_SOURCES=$DATA_DIR/dev/sources.txt
 DEV_TARGETS=$DATA_DIR/dev/targets.txt
 TEST_SOURCES=$DATA_DIR/test/sources.txt
 TEST_TARGETS=$DATA_DIR/test/targets.txt
+
 MODEL_DIR=$TASK_ROOT/model_yard
 
 echo "MODEL_DIR: $MODEL_DIR"
@@ -38,7 +42,7 @@ TRAIN_STEPS=500000
 BATCH_SIZE=64
 EVAL_EVERY_N_STEPS=10000
 
-export PYTHONPATH=${PWD}/pyrouge:${SEQ2SEQ_PROJECT_DIR}:$PYTHONPATH
+export PYTHONPATH=${SEQ2SEQ_PROJECT_DIR}:${PYTHONPATH}
 echo "PYTHONPATH: ${PYTHONPATH}"
 
 cd ${SEQ2SEQ_PROJECT_DIR}
@@ -60,23 +64,23 @@ python -m bin.train \
   --eval_every_n_steps=${EVAL_EVERY_N_STEPS} \
   --output_dir=$MODEL_DIR
 
-PRED_DIR=$TASK_ROOT/predict
-mkdir -p $PRED_DIR
+#PRED_DIR=$TASK_ROOT/predict
+#mkdir -p $PRED_DIR
 
-python -m bin.infer \
-  --tasks "
-    - class: DecodeText
-      params:
-        unk_replace: True
-    - class: DumpBeams
-      params:
-        file: ${PRED_DIR}/beams.npz" \
-  --model_dir $MODEL_DIR \
-  --model_params "
-    inference.beam_search.beam_width: 5" \
-  --input_pipeline "
-    class: ParallelTextInputPipeline
-    params:
-      source_files:
-        - $DEV_SOURCES" \
-  > ${PRED_DIR}/predictions.txt
+#python -m bin.infer \
+#  --tasks "
+#    - class: DecodeText
+#      params:
+#        unk_replace: True
+#    - class: DumpBeams
+#      params:
+#        file: ${PRED_DIR}/beams.npz" \
+#  --model_dir $MODEL_DIR \
+#  --model_params "
+#    inference.beam_search.beam_width: 5" \
+#  --input_pipeline "
+#    class: ParallelTextInputPipeline
+#    params:
+#      source_files:
+#        - $DEV_SOURCES" \
+#  > ${PRED_DIR}/predictions.txt
