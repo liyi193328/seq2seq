876f979ea4a65cf8de5d18c9beb84ca1a97fd425
yi li <yi li>
2017-05-29 15:04:16 +0800

    add

--- run_scripts/run_local_dis.sh
+++ run_scripts/run_local_dis.sh
@@ -3,6 +3,10 @@
 DATA_DIR=${data_dir:-"/home/bigdata/active_project/test_seq2seq_py2/yard_seq2seq/data/q2q_12w_cancel_dup"}
 MODEL_NAME=${model_name:-"add_residual_connections"}
 CONFIG_DIR=${conf_dir:-"../example_configs/q2q_12w"}
+echo "data_dir:${DATA_DIR}"
+echo "model_name:${MODEL_NAME}"
+echo "seq2seq_project:${SEQ2SEQ_PROJECT}"
+echo "config_dir:${CONFIG_DIR}"
 SEQ2SEQ_PROJECT=${seq2seq_project:-"/home/bigdata/active_project/seq2seq"}
 export CUDA_VISIBLE_DEVICES=""; python $SEQ2SEQ_PROJECT/bin/train.py --config_path="$CONFIG_DIR/nmt_small.yml, $CONFIG_DIR/train_seq2seq.yml, $CONFIG_DIR/text_metrics_bpe.yml" --ps_hosts="localhost:2222" --worker_hosts="localhost:2223,localhost:2224,localhost:2225" --job_name="ps" --task_index=0 --cloud=True --schedule="default" --output_dir="${DATA_DIR}/model/$MODEL_NAME" --gpu_memory_fraction=1 --eval_every_n_steps=8000 --train_steps=200000 --batch_size=64 --max_--save_checkpoints_secs=1200 --keep_checkpoint_max=10 clear_output_dir=False > ${DATA_DIR}/ps_${MODEL_NAME}.log 2>&1 &
 export CUDA_VISIBLE_DEVICES="0";python $SEQ2SEQ_PROJECT/bin/train.py --config_path="$CONFIG_DIR/nmt_small.yml, $CONFIG_DIR/train_seq2seq.yml, $CONFIG_DIR/text_metrics_bpe.yml" --ps_hosts="localhost:2222" --worker_hosts="localhost:2223,localhost:2224,localhost:2225" --job_name="worker" --task_index=0 --cloud=True --schedule="train" --output_dir="${DATA_DIR}/model/$MODEL_NAME" --gpu_memory_fraction=0.5 --eval_every_n_steps=8000 --train_steps=200000 --batch_size=64 --max_--save_checkpoints_secs=1200 --keep_checkpoint_max=10 --clear_output_dir=False > ${DATA_DIR}/worker0_${MODEL_NAME}.log 2>&1 &
