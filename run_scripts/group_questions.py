f69fed8dead34e385c4ec9b4c451adb20f8f627d
liyi193328 <liyi193328@github.com>
2017-06-12 17:12:29 +0800

    add filter sim

--- run_scripts/group_questions.py
+++ run_scripts/group_questions.py
@@ -4,100 +4,15 @@ import os
 import sys
 import codecs
 import make_seq2seq_data
+import click
 
-
-def get_source_target(qs, keys, xs, ys,keep_one=False):
-  sources = []
-  targets = []
-  for key in keys:
-    vs = qs[key]
-    if keep_one is True:
-      vs = [vs[0]]
-    sources.extend([xs[v] for v in vs])
-    targets.extend([ys[v] for v in vs])
-  return sources, targets
-
-def get_unique_ques(file_path,save_path, sample_size=None, add_dual=True):
-  from os.path import join
-  if os.path.exists(os.path.dirname(save_path)) == False:
-    os.makedirs(os.path.dirname(save_path))
-  lines = codecs.open(file_path, "r", "utf-8").readlines()
-  f = codecs.open(save_path,"w", "utf-8")
-  qs_set = set()
-  for i, line in enumerate(lines):
-    t = line.strip().split("\t")
-    x = t[0].strip()
-    y = t[1].strip()
-    if x not in qs_set:
-      qs_set.add(x)
-      f.write(x + "\n")
-    if add_dual is True:
-      if y not in qs_set:
-        qs_set.add(y)
-        f.write(y+"\n")
-  f.close()
-
-def make(file_path, save_dir, ratio_list=None, keep=None, add_dual=True):
-  from os.path import join
-  if os.path.exists(save_dir) == False:
-    os.makedirs(save_dir)
-
-  lines = codecs.open(file_path,"r","utf-8").readlines()
-  qs = {}
-  xs = []
-  ys = []
-
-  for i, line in enumerate(lines):
-    t = line.strip().split("\t")
-    x = t[0].strip()
-    y = t[1].strip()
-    xs.append(x)
-    ys.append(y)
-    if x not in qs:
-     qs[x] = []
-    if keep is None:
-      qs[x].append(i)
-    else:
-      if len(qs[x]) > keep:
-        continue
-
-  if ratio_list is None:
-   ratio_list = [0.95, 1.0, 1.0]
-
-  q_num = len(qs)
-  ques_list = list(qs.keys())
-  train_index = int(q_num * ratio_list[0])
-  eval_index = int(q_num * ratio_list[1])
-  test_index = int(q_num * ratio_list[2])
-
-  train_dir = join(save_dir, "train")
-  eval_dir = join(save_dir, "dev")
-  test_dir = join(save_dir, "test")
-
-  train_keys, eval_keys, test_keys = ques_list[0:train_index], \
-                                     ques_list[train_index:eval_index],\
-                                     ques_list[eval_index:test_index]
-
-  train_s, train_t = get_source_target(qs, train_keys, xs, ys)
-  eval_s, eval_t = get_source_target(qs, eval_keys, xs, ys, keep_one=True)
-  test_s, test_t = get_source_target(qs, test_keys, xs, ys, keep_one=True)
-
-  if add_dual:
-    train_s.extend(train_t)
-    train_t.extend(train_s)
-    eval_s.extend(eval_t)
-    eval_t.extend(eval_s)
-    test_s.extend(test_t)
-    test_t.extend(test_s)
-
-  make_seq2seq_data.write_some_data(train_s, train_t, train_dir)
-  make_seq2seq_data.write_some_data(eval_s, eval_t, eval_dir)
-  make_seq2seq_data.write_some_data(test_s, test_t, test_dir)
 
 
 if __name__ == "__main__":
+  '''
   make("../data/q2q_pos.train", "../data/q2q_12w_cancel_dup_dual", keep=None, add_dual=True)
   get_unique_ques("../data/q2q_pos.train", "../data/all_ques.txt", add_dual=True)
-
+  '''
+  make()
 
 
